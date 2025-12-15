2f692f900c50329fefff91c2d4c7440e28f952b6
TristanTheart <tristantheart@gmail.com>
2025-12-15 11:50:15 +0200

    added widgets for for quiz screen and conditional state functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_basics/quiz.dart';
+
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    const appTitle = 'Flutter Quiz';
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(title: const Text(appTitle)),
-          body: Center(
-            child: Text(
-              style: const TextStyle(
-                fontSize: 20,
-                color: Colors.white),
-                'Learn Flutter the fun way!',
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(const Quiz());
 }
